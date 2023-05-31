97cb2ca2b71b63c7b45e9994a29b3dff8c315991
Him Daoud <56063700+HimDaoud@users.noreply.github.com>
2023-05-31 05:17:43 +0200

    Update

---
+++ KundeApp2/Controllers/KundeController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KundeApp2.DAL;
+using KundeApp2.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KundeApp2.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class KundeController : ControllerBase
+    {
+        private IKundeRepository _db;
+
+        private ILogger<KundeController> _log;
+
+        private const string _loggetInn = "loggetInn";
+
+        public KundeController(IKundeRepository db, ILogger<KundeController> log)
+        {
+            _db = db;
+            _log = log;
+        }
+
+        public async Task<ActionResult> Lagre(Kunde innKunde)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(_loggetInn)))
+            {
+                return Unauthorized();
+            }
+            if (ModelState.IsValid)
+            {
+                bool returOK = await _db.Lagre(innKunde);
+                if (!returOK)
+                {
+                    _log.LogInformation("Kunden kunne ikke lagres!");
+                    return BadRequest("Kunden kunne ikke lagres");
+                }
+                return Ok("Kunde lagret");
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering på server");
+        }
+
+        public async Task<ActionResult> HentAlle()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(_loggetInn)))
+            {
+                return Unauthorized();
+            }
+            List<Kunde> alleKunder = await _db.HentAlle();
+            return Ok(alleKunder); 
+        }
+
+        public async Task<ActionResult> Slett(int id)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(_loggetInn)))
+            {
+                return Unauthorized();
+            }
+            bool returOK = await _db.Slett(id);
+            if (!returOK)
+            {
+                _log.LogInformation("Sletting av Kunden ble ikke utført");
+                return NotFound("Sletting av Kunden ble ikke utført");
+            }
+            return Ok("Kunde slettet");
+        }
+
+        public async Task<ActionResult> HentEn(int id)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(_loggetInn)))
+            {
+                return Unauthorized();
+            }
+            if (ModelState.IsValid)
+            {
+                Kunde kunden = await _db.HentEn(id);
+                if (kunden == null)
+                {
+                    _log.LogInformation("Fant ikke kunden");
+                    return NotFound("Fant ikke kunden");
+                }
+                return Ok(kunden);
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering på server");
+        }
+
+        public async Task<ActionResult> Endre(Kunde endreKunde)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(_loggetInn)))
+            {
+                return Unauthorized();
+            }
+            if (ModelState.IsValid)
+            {
+                bool returOK = await _db.Endre(endreKunde);
+                if (!returOK)
+                {
+                    _log.LogInformation("Endringen kunne ikke utføres");
+                    return NotFound("Endringen av kunden kunne ikke utføres");
+                }
+                return Ok("Kunde endret");
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering på server");
+        }
+        public async Task<ActionResult> LoggInn(Bruker bruker) 
+        {
+            if (ModelState.IsValid)
+            {
+                bool returnOK = await _db.LoggInn(bruker);
+                if (!returnOK)
+                {
+                    _log.LogInformation("Innloggingen feilet for bruker"+bruker.Brukernavn);
+                    HttpContext.Session.SetString(_loggetInn,"");
+                    return Ok(false);
+                }
+                HttpContext.Session.SetString(_loggetInn, "LoggetInn");
+                return Ok(true);
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering på server");
+        }
+
+        public void LoggUt()
+        {
+            HttpContext.Session.SetString(_loggetInn,"");
+        }
+    }
+}
+
+    
